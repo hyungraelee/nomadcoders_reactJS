ee60bae5ed6c2b8a2e6e0cbacc8b328f2128f5d4
hyungraelee <timgg12345@gmail.com>
2021-08-05 16:04:56 +0900

    learn react class component & state

--- src/App.js
+++ src/App.js
@@ -1,46 +1,26 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-function Food({name, start, rating}) {
-	return (
-	<h2>start {start}, {name}, rating: {rating}/5.0</h2>
-	);
-}
-
-Food.propTypes = {
-	name: PropTypes.string.isRequired,
-	start: PropTypes.string.isRequired,
-	rating: PropTypes.number.isRequired
-};
-
-const foodILike = [
-	{
-		id: 1,
-		start: "k",
-		name: "kimchi",
-		rating: 4.2
-	},
-	{
-		id: 2,
-		start: "r",
-		name: "ramen",
-		rating: 4.7
-	},
-	{
-		id: 3,
-		start: "c",
-		name: "coffee",
-		rating: 4.9
+class App extends React.Component{
+	state = {
+		count: 0
+	};
+	add = () => {
+		this.setState(current => ({ count: current.count + 1 }));
+	};
+	minus = () => {
+		this.setState(current => ({ count: current.count - 1 }));
+	};
+	render(){
+		return (
+			<div>
+				<h1>The number is {this.state.count}</h1>
+				<button onClick={this.add}>Add</button>
+				<button onClick={this.minus}>Minus</button>
+			</div>
+		);
 	}
-];
 
-function App() {
-	return (
-		<div className="App">
-			<h1>Hello</h1>
-			{foodILike.map(dish => (<Food key={dish.id} name={dish.name} start={dish.start} rating={dish.rating} />))}
-		</div>
-	);
 }
 
 export default App;
